52fa5424c9bcf4260b3635602d94b7a7acb53e6b
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-09 11:08:11 +0100

    icones + colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: '#9E2384',
+        blue: '#2B0D84',
+        orange: '#D7964E',
+        gold: '#8E5C0E',
+        dark: '#25252F',
+        black: '#181619'
+      }
+    },
   },
   plugins: [],
 }
