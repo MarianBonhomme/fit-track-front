d8de18d1611f7a93b9c9f755c2c0a96d69a70c2a
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-28 18:35:33 +0100

    style(foodCard): ajout menu hamburger

--- src/components/nutrition/myfoods/FoodCard.jsx
+++ src/components/nutrition/myfoods/FoodCard.jsx
@@ -1,34 +1,71 @@
 import { Icon } from '@iconify/react';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNutrition } from '../../../utils/NutritionContext';
 import FoodImage from '../global/FoodImage';
 import MacrosQuantities from '../global/MacrosQuantities';
 
 function FoodCard({food, editBtnClicked}) {
   const { handleUpdateFood } = useNutrition();
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+  
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+  }, [isOpen]);
 
   const addToFavorite = (food) => {
     const foodFavorite = { ...food, is_favorite: 1, is_active: 1, };
     handleUpdateFood(foodFavorite);
+    setIsOpen(false);
   }
 
   const removeFromFavorite = (food) => {
     const foodNotFavorite = { ...food, is_favorite: 0 };
     handleUpdateFood(foodNotFavorite);
+    setIsOpen(false);
   }
 
   const setActive = (food) => {
     const foodActive = { ...food, is_active: 1 };
     handleUpdateFood(foodActive);
+    setIsOpen(false);
   }
 
   const setInactive = (food) => {
     const foodInactive = { ...food, is_active: 0, is_favorite : 0 };
     handleUpdateFood(foodInactive);
+    setIsOpen(false);
   }
   
   return (
-    <div className={`bg-primary relative w-[300px] mt-[50px] pt-[80px] flex flex-col justify-between text-center shadow-custom rounded-2xl p-4 ${!food.is_active && 'opacity-60'}`}>
+    <div ref={ref} className={`bg-primary relative w-[300px] mt-[50px] pt-[80px] flex flex-col justify-between text-center shadow-custom rounded-2xl p-4 ${!food.is_active && 'opacity-60'}`}>
+      <div className={`absolute -top-5 -right-5 bg-primary rounded-2xl p-3 z-50 shadow-custom ${isOpen ? '' : 'hidden'}`}>
+        <div className="flex items-center gap-1 cursor-pointer" onClick={editBtnClicked} >
+          <Icon icon="mage:settings-fill" width={30} height={30} className="text-blue cursor-pointer" />
+          <p className='font-bold'>Modifier</p>
+        </div>
+        {food.is_active ? (  
+          <div className="flex items-center gap-1 cursor-pointer" onClick={() => setInactive(food)} >       
+            <Icon icon="ic:round-delete" width={30} height={30} className="text-red cursor-pointer"/>
+            <p className='font-bold'>Désactiver</p>      
+          </div>  
+        ) : (
+          <div className="flex items-center gap-1 cursor-pointer" onClick={() => setActive(food)} >
+            <Icon icon="mingcute:arrow-up-fill" width={30} height={30} className="text-purple cursor-pointer" /> 
+            <p className='font-bold'>Activer</p> 
+          </div>    
+        )}
+      </div>
       <div className="absolute -top-[20px] left-0 w-full flex justify-between items-center px-3">
         {food.is_favorite ? (         
           <Icon icon="solar:star-bold" width={30} height={30} className="text-yellow cursor-pointer" onClick={() => removeFromFavorite(food)} />         
@@ -36,14 +73,7 @@ function FoodCard({food, editBtnClicked}) {
           <Icon icon="solar:star-bold" width={30} height={30} className="text-gray cursor-pointer" onClick={() => addToFavorite(food)} />         
         )}
         <FoodImage image={food.image} size="xl" />
-        <div className='pt-8'>
-          <Icon icon="mage:settings-fill" width={30} height={30} className="text-blue cursor-pointer" onClick={editBtnClicked} />
-          {food.is_active ? (         
-            <Icon icon="ic:round-delete" width={30} height={30} className="text-red cursor-pointer" onClick={() => setInactive(food)} />        
-          ) : (
-            <Icon icon="mingcute:arrow-up-fill" width={30} height={30} className="text-purple cursor-pointer" onClick={() => setActive(food)} />      
-          )}
-        </div>
+        <Icon icon="solar:menu-dots-bold" width="30" height="30" className='text-blue cursor-pointer' onClick={() => setIsOpen(true)} />
       </div>
       <h3 className="text-xl font-bold my-3">{food.name}</h3>	
       <MacrosQuantities macros={food} />
