91e4f88ec9a0976405dc994a48855e5c15203005
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-30 07:14:04 +0100

    style(foodCard): favoris

--- src/components/nutrition/myfoods/FoodCard.jsx
+++ src/components/nutrition/myfoods/FoodCard.jsx
@@ -50,6 +50,17 @@ function FoodCard({food, editBtnClicked}) {
   return (
     <div ref={ref} className={`bg-primary relative w-[300px] mt-[50px] pt-[80px] shadow-custom rounded-2xl p-4 ${!food.is_active && 'opacity-60'}`}>
       <div className={`absolute -top-5 -right-5 bg-primary rounded-2xl p-3 z-50 shadow-custom ${isOpen ? '' : 'hidden'}`}>
+        {food.is_favorite ? (        
+          <div className="flex items-center gap-1 cursor-pointer" >
+            <Icon icon="solar:star-bold" width={30} height={30} className="text-yellow cursor-pointer" onClick={() => removeFromFavorite(food)} />         
+            <p className='font-bold'>Favoris</p>
+          </div> 
+        ) : (
+          <div className="flex items-center gap-1 cursor-pointer" >
+            <Icon icon="solar:star-bold" width={30} height={30} className="text-gray cursor-pointer" onClick={() => addToFavorite(food)} />         
+            <p className='font-bold'>Favoris</p>
+          </div>
+        )}
         <div className="flex items-center gap-1 cursor-pointer" onClick={editBtnClicked} >
           <Icon icon="mage:settings-fill" width={30} height={30} className="text-blue cursor-pointer" />
           <p className='font-bold'>Edit</p>
