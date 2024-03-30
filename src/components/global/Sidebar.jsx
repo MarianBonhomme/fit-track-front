8f9d6103328bf90a04cdf309156a3415df939f9f
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-30 07:07:50 +0100

    fix(routing): sidebar nutrition link

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
   return (
     <nav className="h-screen flex flex-col justify-between items-center py-10">
       <div className="flex flex-col gap-10">
-        <NavLink to="nutrition" className="relative">
+        <NavLink to="/" className="relative">
           <Icon icon="fa-solid:apple-alt" width={40} height={40} className="text-green cursor-pointer" />
         </NavLink>
         {/* <NavLink to="sport" className="relative">
