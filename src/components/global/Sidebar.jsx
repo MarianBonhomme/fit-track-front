79f460c63eacad6b8664d8fc4b1d73789ae16432
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-04-06 17:29:06 +0200

    feat: signout

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -1,8 +1,10 @@
 import { Icon } from "@iconify/react";
 import { useDashboard } from "../../utils/DashboardContext";
+import { useUser } from "../../utils/UserContext";
 
 export default function Sidebar() {
   const { setActiveDashboard } = useDashboard();
+  const { user, handleSignout } = useUser();
 
   return (
     <nav className="h-screen flex flex-col justify-between items-center py-10">
@@ -18,8 +20,17 @@ export default function Sidebar() {
           icon="material-symbols:fitness-center-rounded"
           width={40}
           height={40}
-          className="text-red cursor-pointer"
+          className="text-purple cursor-pointer"
           onClick={() => setActiveDashboard("sport")}
+        />
+      </div>
+      <div className="flex flex-col gap-10">
+        <Icon
+          icon="uil:signout"
+          width={40}
+          height={40}
+          className="text-red cursor-pointer"
+          onClick={handleSignout}
         />
       </div>
     </nav>
