595c1a984cf64aa7f20096333e6a4d41de072ea3
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-04-21 09:52:44 +0200

    feat(dateService): getFullDate & getShortDate

--- src/utils/global/DateService.jsx
+++ src/utils/global/DateService.jsx
@@ -1,17 +1,19 @@
-const getFormattedDate = (date) => {
+const getFullDate = (date) => {
   if (isToday(date)) {
     return "Today";
   } 
   
+  const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const dateOrdinals = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
 
-  const dayOfWeek = daysOfWeek[date.getDay()];
+  const monthOfYear = monthsOfYear[date.getMonth()];
   const dayOfMonth = date.getDate();
+  const dayOfWeek = daysOfWeek[date.getDay()];
 
   const dateOrdinal = dateOrdinals[dayOfMonth % 10 === 1 && dayOfMonth !== 11 ? 1 : dayOfMonth % 10];
 
-  return `${dayOfWeek} ${dayOfMonth}${dateOrdinal}`;
+  return `${dayOfWeek}, ${monthOfYear} ${dayOfMonth}${dateOrdinal}`;
 };
 
 const isToday = (date) => {
@@ -19,5 +21,18 @@ const isToday = (date) => {
   return date.getDate() === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()
 }
 
+const getShortDate = (date) => {
+  let month = (date.getMonth() + 1).toString();
+  let day = date.getDate().toString();
+  
+  if (month.length === 1) {
+    month = '0' + month;
+  }
+  if (day.length === 1) {
+    day = '0' + day;
+  }
+  
+  return `${day}/${month}`;
+}
 
-export { getFormattedDate };
+export { getFullDate, getShortDate };
