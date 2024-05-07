e67a64378cb03104663dbafb9caabe8f47950fc6
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-07 16:06:37 +0200

    responsive userDashboard

--- src/components/user/UserDashboard.jsx
+++ src/components/user/UserDashboard.jsx
@@ -31,53 +31,51 @@ export default function UserDashboard() {
     return color
   }
 
-  return (
-    <div className="p-5 space-y-5">   
-      <div className="grid grid-cols-2 gap-5">
-        <div className='grid grid-cols-2 gap-5'>
-          <div className='flex flex-col items-center justify-between bg-primary rounded-3xl p-5'>
-            <CardTitle text="Current Profile"/>
-            <div className='space-y-3 text-center'>
-              <AvatarColor avatar={profileAvatar} color={profileColor} colorSize={"3xl"} avatarSize={"xl"} />
-              <p className='font-bold'>{profile.pseudo}</p>
-            </div>
-            <DarkModeSwitch
-              checked={isDarkMode}
-              onChange={toggleDarkMode}
-              size={50}
-            />
+  return ( 
+    <div className="grid sm:grid-cols-2 gap-3 sm:gap-5 p-3 sm:p-5">
+      <div className='grid sm:grid-cols-2 gap-3 sm:gap-5'>
+        <div className='flex flex-col items-center max-sm:gap-5 justify-between bg-primary rounded-3xl p-5'>
+          <CardTitle text="Current Profile"/>
+          <div className='space-y-3 text-center'>
+            <AvatarColor avatar={profileAvatar} color={profileColor} colorSize={"3xl"} avatarSize={"xl"} />
+            <p className='font-bold'>{profile.pseudo}</p>
           </div>
-          <div className="flex flex-col items-center justify-between bg-primary rounded-3xl p-5">
-            <CardTitle text={"Daily Macros Goals"} />
-            <div>
-              <CardTitle text={"Kcal"} />
-              {profile.dailyKcal}
-            </div>
-            <div>
-              <CardTitle text={"Prot"} />
-              {profile.dailyProt}
-            </div>
-            <div>
-              <CardTitle text={"Fat"} />
-              {profile.dailyFat}
-            </div>
-            <div>
-              <CardTitle text={"Carb"} />
-              {profile.dailyCarb}
-            </div>
+          <DarkModeSwitch
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+            size={50}
+          />
+        </div>
+        <div className="flex flex-col items-center justify-between bg-primary rounded-3xl p-5">
+          <CardTitle text={"Daily Macros Goals"} />
+          <div>
+            <CardTitle text={"Kcal"} />
+            {profile.dailyKcal}
           </div>
-        </div> 
-        <div className='w-full flex flex-col gap-20'>
-          <div className='flex flex-wrap gap-10'>
-            {avatars && avatars.map((avatar) => (
-              <AvatarItem key={avatar.id} avatar={avatar} clicked={() => changeAvatar(avatar)} size={'lg'} />
-            ))}
+          <div>
+            <CardTitle text={"Prot"} />
+            {profile.dailyProt}
           </div>
-          <div className='flex flex-wrap gap-10'>
-            {colors && colors.map((color) => (
-              <ColorItem key={color.id} color={color} clicked={() => changeColor(color)} size={'lg'} />
-            ))}
+          <div>
+            <CardTitle text={"Fat"} />
+            {profile.dailyFat}
           </div>
+          <div>
+            <CardTitle text={"Carb"} />
+            {profile.dailyCarb}
+          </div>
+        </div>
+      </div> 
+      <div className='w-full flex flex-col gap-5 sm:gap-20'>
+        <div className='flex flex-wrap gap-5 sm:gap-10'>
+          {avatars && avatars.map((avatar) => (
+            <AvatarItem key={avatar.id} avatar={avatar} clicked={() => changeAvatar(avatar)} size={'lg'} />
+          ))}
+        </div>
+        <div className='flex flex-wrap gap-5 sm:gap-10'>
+          {colors && colors.map((color) => (
+            <ColorItem key={color.id} color={color} clicked={() => changeColor(color)} size={'lg'} />
+          ))}
         </div>
       </div>
     </div>
