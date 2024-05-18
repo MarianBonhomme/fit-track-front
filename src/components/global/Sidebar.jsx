447dcb56dd3bf65b233e133e84e12dc6ab379c7c
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-18 17:34:53 +0200

    style sidebar

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -1,27 +1,32 @@
 import { Icon } from "@iconify/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
+  const location = useLocation();
+
   return (
-    <nav className="h-[70px] sm:h-screen max-sm:w-screen sm:w-[80px] bg-primary fixed bottom-0 sm:top-0 left-0 z-40 flex sm:flex-col max-sm:justify-evenly sm:gap-10 items-center sm:py-10">
-      <Link to='/profile'>
-        <Icon
-          icon="fluent:settings-24-filled"
-          className="text-secondary cursor-pointer size-[35px]"
-        />   
-      </Link>
-      <Link to='/nutrition'>
-        <Icon
-          icon="fa-solid:apple-alt"
-          className="text-green cursor-pointer size-[35px]"
-        />
-      </Link>
-      <Link to='/sport'>
-        <Icon
-          icon="material-symbols:fitness-center-rounded"
-          className="text-purple cursor-pointer size-[35px]"
-        />
-      </Link>
+    <nav className="h-[60px] sm:h-screen max-sm:w-screen sm:w-[80px] bg-primary fixed bottom-0 sm:top-0 left-0 z-40 flex sm:flex-col max-sm:justify-evenly sm:gap-10 items-center sm:py-10">
+      {location.pathname === "/nutrition" ? (
+        <Icon icon="ion:nutrition" className="text-secondary size-[30px]"/>
+      ) : (
+        <Link to='/nutrition'>
+          <Icon icon="ion:nutrition-outline" className="text-secondary size-[30px]" />
+        </Link>
+      )}
+      {location.pathname === "/profile" ? (
+        <Icon icon="iconamoon:profile-fill" className="text-secondary size-[30px]" />
+      ) : (
+        <Link to='/profile'>
+          <Icon icon="iconamoon:profile-light" className="text-secondary size-[30px]" />
+        </Link>
+      )}
+      {location.pathname === "/sport" ? (
+        <Icon icon="ion:fitness" className="text-secondary size-[30px]"/>
+      ) : (
+        <Link to='/sport'>
+          <Icon icon="ion:fitness-outline" className="text-secondary size-[30px]" />
+        </Link>
+      )}
     </nav>
   );
 }
