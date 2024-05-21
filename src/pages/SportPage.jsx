53c814e8763919be687234ebcfcf08be6e741816
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-21 15:51:48 +0200

    feat/fix: sportLoading (buf refresh)

--- src/pages/SportPage.jsx
+++ src/pages/SportPage.jsx
@@ -11,47 +11,49 @@ import { useSport } from '../utils/sport/SportContext';
 
 export default function SportPage() {
   const { isDarkMode, toggleDarkMode } = useUser();
-  const { isTrainingFormDisplayed, isProgramFormDisplayed, openProgramForm } = useSport();
+  const { isTrainingFormDisplayed, isProgramFormDisplayed, openProgramForm, sportLoading } = useSport();
 
   const tabs = ['programs', 'calendar'];
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   return (
-    <>
-      <div className='p-3 sm:p-5'>
-        <div className='flex justify-between items-start'>
-          <ul className='flex font-bold'>
-            {tabs.map((tab, index) => (
-              <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6 flex gap-3 items-center`} onClick={() => setActiveTab(tab)} >
-                {tab.charAt(0).toUpperCase() + tab.slice(1)}
-                {tab === 'programs' && <AddButton css={'w-6 h-6 p-2'} clicked={openProgramForm} />}
-              </li>
-            ))}
-          </ul>
-          <div className='sm:flex items-center justify-center gap-3'>
-            <div className='max-sm:hidden'>
-              <DarkModeSwitch
-                checked={isDarkMode}
-                onChange={toggleDarkMode}
-                size={30}
-              />
+    !sportLoading && (
+      <>
+        <div className='p-3 sm:p-5'>
+          <div className='flex justify-between items-start'>
+            <ul className='flex font-bold'>
+              {tabs.map((tab, index) => (
+                <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6 flex gap-3 items-center`} onClick={() => setActiveTab(tab)} >
+                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                  {tab === 'programs' && <AddButton css={'w-6 h-6 p-2'} clicked={openProgramForm} />}
+                </li>
+              ))}
+            </ul>
+            <div className='sm:flex items-center justify-center gap-3'>
+              <div className='max-sm:hidden'>
+                <DarkModeSwitch
+                  checked={isDarkMode}
+                  onChange={toggleDarkMode}
+                  size={30}
+                />
+              </div>
+              <ProfileIcon />
             </div>
-            <ProfileIcon />
           </div>
+          {activeTab === 'calendar' && (
+            <CalendarTab />
+          )}
+          {activeTab === 'programs' && (
+            <ProgramsTab />
+          )}
         </div>
-        {activeTab === 'calendar' && (
-          <CalendarTab />
+        {isProgramFormDisplayed && (
+          <ProgramForm />
+        )} 
+        {isTrainingFormDisplayed && (
+          <TrainingForm />
         )}
-        {activeTab === 'programs' && (
-          <ProgramsTab />
-        )}
-      </div>
-      {isProgramFormDisplayed && (
-        <ProgramForm />
-      )} 
-      {isTrainingFormDisplayed && (
-        <TrainingForm />
-      )}
     </>
+    )
   );
 }
