d3893ebf6c6d1762acab9b27c45ea5b156a0e426
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-21 16:03:13 +0200

    style: pages

--- src/pages/NutritionPage.jsx
+++ src/pages/NutritionPage.jsx
@@ -15,24 +15,22 @@ export default function NutritionPage() {
 
   return (
     !nutritionLoading && (
-      <div className='p-3 sm:p-5'>
-        <div className='flex justify-between'>
-          <ul className='flex font-bold'>
-            {tabs.map((tab, index) => (
-              <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6`} onClick={() => setActiveTab(tab)} >{tab.charAt(0).toUpperCase() + tab.slice(1)}</li>
-            ))}
-          </ul>
-          <div className='sm:flex items-center justify-center gap-3'>
-            <div className='max-sm:hidden'>
-              <DarkModeSwitch
-                checked={isDarkMode}
-                onChange={toggleDarkMode}
-                size={30}
-              />
-            </div>
-            <ProfileIcon />
+      <div className='p-3 sm:p-5 relative'>
+        <div className='absolute right-3 top-3 sm:right-5 sm:top-5 flex items-center justify-center gap-3'>
+          <div>
+            <DarkModeSwitch
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+              size={30}
+            />
           </div>
+          <ProfileIcon />
         </div>
+        <ul className='flex font-bold'>
+          {tabs.map((tab, index) => (
+            <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6`} onClick={() => setActiveTab(tab)} >{tab.charAt(0).toUpperCase() + tab.slice(1)}</li>
+          ))}
+        </ul>
         {activeTab === 'dashboard' && (
           <NutritionDashboardTab />
         )}
--- src/pages/SportPage.jsx
+++ src/pages/SportPage.jsx
@@ -19,27 +19,25 @@ export default function SportPage() {
   return (
     !sportLoading && (
       <>
-        <div className='p-3 sm:p-5'>
-          <div className='flex justify-between items-start'>
-            <ul className='flex font-bold'>
-              {tabs.map((tab, index) => (
-                <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6 flex gap-3 items-center`} onClick={() => setActiveTab(tab)} >
-                  {tab.charAt(0).toUpperCase() + tab.slice(1)}
-                  {tab === 'programs' && <AddButton css={'w-6 h-6 p-2'} clicked={openProgramForm} />}
-                </li>
-              ))}
-            </ul>
-            <div className='sm:flex items-center justify-center gap-3'>
-              <div className='max-sm:hidden'>
-                <DarkModeSwitch
-                  checked={isDarkMode}
-                  onChange={toggleDarkMode}
-                  size={30}
-                />
-              </div>
-              <ProfileIcon />
+        <div className='p-3 sm:p-5 relative'>
+          <div className='absolute right-3 top-3 sm:right-5 sm:top-5 flex items-center justify-center gap-3'>
+            <div>
+              <DarkModeSwitch
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+                size={30}
+              />
             </div>
+            <ProfileIcon />
           </div>
+          <ul className='flex font-bold'>
+            {tabs.map((tab, index) => (
+              <li key={index} className={`${activeTab === tab ? 'bg-primary' : 'cursor-pointer'} text-xs rounded-t-2xl py-3 px-6 flex gap-3 items-center`} onClick={() => setActiveTab(tab)} >
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                {tab === 'programs' && <AddButton css={'w-6 h-6 p-2'} clicked={openProgramForm} />}
+              </li>
+            ))}
+          </ul>
           {activeTab === 'calendar' && (
             <CalendarTab />
           )}
