018bd8c9dc3b648505d1a7b788505d41af42d022
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-30 15:02:26 +0200

    style: responsive userPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -15,21 +15,23 @@ export default function UserPage() {
   return ( 
     !userLoading && 
       <>
-        <div className='grid sm:grid-cols-3 gap-10 p-3 sm:p-5 relative'>
-          <div className="flex justify-between items-center">
-            <Icon icon="majesticons:logout-half-circle" className='text-red size-[30px] cursor-pointer' onClick={handleSignout} />
-            <Icon icon="akar-icons:edit" className='size-[30px]' onClick={() => setIsEditUserModalVisible(true)} />
-            <DarkModeSwitch
-              checked={isDarkMode}
-              onChange={toggleDarkMode}
-              size={30}
-            />
+        <div className='grid sm:max-w-xl gap-10 p-3 sm:p-5 relative'>
+          <div className='w-full flex flex-col gap-10 sm:bg-primary sm:rounded-3xl sm:p-5'>
+            <div className="flex justify-between items-center">
+              <Icon icon="majesticons:logout-half-circle" className='text-red size-[30px] cursor-pointer' onClick={handleSignout} />
+              <Icon icon="akar-icons:edit" className='size-[30px]' onClick={() => setIsEditUserModalVisible(true)} />
+              <DarkModeSwitch
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+                size={30}
+              />
+            </div>
+            <div className='flex flex-col items-center gap-1'>
+              <AvatarColor avatar={user.avatar} color={user.color} colorSize={"4xl"} avatarSize={"xl"} />
+              <p className='text-2xl font-bold'>{user.pseudo}</p>
+            </div>
           </div>
-          <div className='flex flex-col items-center gap-1'>
-            <AvatarColor avatar={user.avatar} color={user.color} colorSize={"4xl"} avatarSize={"xl"} />
-            <p className='text-2xl font-bold'>{user.pseudo}</p>
-          </div>
-          <div className="grid grid-cols-2 mx-auto gap-5">
+          <div className="grid grid-cols-2 sm:grid-cols-4 max-sm:mx-auto gap-5">
             {macros.map((macro) => (
               <DailyMacroGoal key={macro} value={user[`daily${macro.charAt(0).toUpperCase()}${macro.slice(1)}`]} macro={macro} />
             ))}
